0d04eaccd9f8624934d4034b6baa06b06813195a
moulika-nagulavancha <moulika152@gmail.com>
2023-07-29 17:58:33 -0500

    Docker changes

--- mastering-microservices-with-spring-cloud/currency-exchange-service/src/main/java/com/learn/microservices/currencyexchangeservice/controller/CurrencyExchangeController.java
+++ mastering-microservices-with-spring-cloud/currency-exchange-service/src/main/java/com/learn/microservices/currencyexchangeservice/controller/CurrencyExchangeController.java
@@ -2,6 +2,8 @@ package com.learn.microservices.currencyexchangeservice.controller;
 
 import com.learn.microservices.currencyexchangeservice.bean.CurrencyExchange;
 import com.learn.microservices.currencyexchangeservice.repository.CurrencyExchangeRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,8 +18,11 @@ public class CurrencyExchangeController {
     @Autowired
     private CurrencyExchangeRepository repository;
 
+    private Logger logger = LoggerFactory.getLogger(CurrencyExchangeController.class);
+
     @GetMapping("/currency-exchange/from/{from}/to/{to}")
     public CurrencyExchange retrieveExchangeValue(@PathVariable String from, @PathVariable String to) {
+        logger.info("retrieve exchange value called with {} to {}", from, to);
         CurrencyExchange exchange = repository.findByFromAndTo(from, to);
 
         if (exchange == null) {
